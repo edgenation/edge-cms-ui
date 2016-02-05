aabd001cf7eb7ebdd6b663dea1ea927a673fe1ae
Chris Sheppard <chris.sheppard@thebioagency.com>
2016-02-05 12:19:18 +0000

    Adding redux-devtools-extension support https://github.com/zalmoxisus/redux-devtools-extension

--- src/cms/core/store.js
+++ src/cms/core/store.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import multi from "redux-multi";
 
-const createStoreWithMiddleware = applyMiddleware(thunk, multi)(createStore);
+const createStoreWithMiddleware = compose(
+    applyMiddleware(thunk, multi),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore);
 
 export default function makeStore(reducer) {
     return createStoreWithMiddleware(reducer);
