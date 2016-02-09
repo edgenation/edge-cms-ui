3eeeef6b8870ee50e1d490ea2dd434eb60ad14e2
Chris Sheppard <chris.sheppard@thebioagency.com>
2016-02-09 14:14:51 +0000

    Removing unused debug actor

--- src/cms/actors/index.js
+++ src/cms/actors/index.js
@@ -1,12 +1,10 @@
 import renderer from "./renderer"
 import page from "./page"
 import pages from "./pages"
-import debug from "./debug"
 
 
 export default [
     page,
     pages,
-    debug,
     renderer
 ];
